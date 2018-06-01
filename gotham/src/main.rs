8c4b70a0f96692070fdd3167242509822501be18
Roberto Huertas <roberto.huertas@outlook.com>
2018-06-01 19:15:13 +0200

    added gotham

---
+++ gotham/src/main.rs
@@ -0,0 +1,23 @@
+extern crate gotham;
+extern crate hyper;
+extern crate mime;
+
+use hyper::{Response, StatusCode};
+
+use gotham::http::response::create_response;
+use gotham::state::State;
+
+pub fn say_hello(state: State) -> (State, Response) {
+    let res = create_response(
+        &state,
+        StatusCode::Ok,
+        Some((String::from("Hello World").into_bytes(), mime::TEXT_PLAIN)),
+    );
+    (state, res)
+}
+
+fn main() {
+    let addr = "0.0.0.0:7878";
+    println!("Listening for requests at http://{}", addr);
+    gotham::start(addr, || Ok(say_hello))
+}
