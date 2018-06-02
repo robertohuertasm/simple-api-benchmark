2783686204c9fcd2339f247b3b1f155575c45b5b
Roberto Huertas <roberto.huertas@outlook.com>
2018-06-02 11:22:11 +0200

    added hapi.js

---
+++ hapijs/index.js
@@ -0,0 +1,24 @@
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+  host: 'localhost',
+  port: 3200,
+});
+
+server.route({
+  method: 'GET',
+  path:'/',
+  handler: (request, h) => 'Hello World',
+});
+
+async function start() {
+  try {
+    await server.start();
+  } catch (error) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log('Server running at:', server.info.uri);
+}
+
+start();
--- koajs/index.js
+++ koajs/index.js
@@ -1,7 +1,7 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(ctx => {
+app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
