bb3b26d08ca54e801cb45d119a73f55dc7ec81aa
Roberto Huertas <roberto.huertas@outlook.com>
2018-06-02 10:28:37 +0200

    new actix version

--- actix-api/src/main.rs
+++ actix-api/src/main.rs
@@ -1,9 +1,13 @@
 extern crate actix_web;
-use actix_web::*;
+use actix_web::{server, App, HttpRequest, Responder};
+
+fn greet(_req: HttpRequest) -> impl Responder {
+    "Hello World"
+}
 
 fn main() {
-    HttpServer::new(|| Application::new().resource("/", |r| r.f(|_| "Hello World")))
+    server::new(|| App::new().resource("/", |r| r.f(greet)))
         .bind("0.0.0.0:8888")
-        .unwrap()
+        .expect("Can not bind to port 8888")
         .run();
 }
